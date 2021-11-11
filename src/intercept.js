ee987e8666af79edb03d973e89d60678a0444e48
Eduard Chyzhyk <e.chyzhyk@mageworx.com>
2021-11-11 11:49:30 +0100

    test

--- src/intercept.js
+++ src/intercept.js
@@ -9,31 +9,31 @@ module.exports = targets => {
     // product
     talonsTarget.tap((talonWrapperConfig) => {
         talonWrapperConfig.RootComponents.Product.useProduct.wrapWith(
-            "@mageworx/GiftCards-veniapwa/src/features/Product/modify/targets/wrapUseProduct"
+            "@mageworx/gift-cards-veniapwa/src/features/Product/modify/targets/wrapUseProduct"
         );
     });
     talonsTarget.tap((talonWrapperConfig) => {
         talonWrapperConfig.ProductFullDetail.useProductFullDetail.wrapWith(
-            "@mageworx/GiftCards-veniapwa/src/features/Product/modify/targets/wrapUseProductFullDetails"
+            "@mageworx/gift-cards-veniapwa/src/features/Product/modify/targets/wrapUseProductFullDetails"
         );
     });
 
     // cart
     talonsTarget.tap((talonWrapperConfig) => {
         talonWrapperConfig.CartPage.PriceSummary.usePriceSummary.wrapWith(
-            "@mageworx/GiftCards-veniapwa/src/features/Cart/modify/targets/wrapUsePriceSummary"
+            "@mageworx/gift-cards-veniapwa/src/features/Cart/modify/targets/wrapUsePriceSummary"
         );
     });
 
     // category
     talonsTarget.tap((talonWrapperConfig) => {
         talonWrapperConfig.Gallery.useAddToCartButton.wrapWith(
-            "@mageworx/GiftCards-veniapwa/src/features/Category/modify/targets/wrapUseAddToCartButton"
+            "@mageworx/gift-cards-veniapwa/src/features/Category/modify/targets/wrapUseAddToCartButton"
         );
     });
     talonsTarget.tap((talonWrapperConfig) => {
         talonWrapperConfig.Gallery.useGalleryItem.wrapWith(
-            "@mageworx/GiftCards-veniapwa/src/features/Category/modify/targets/wrapUseGalleryItem"
+            "@mageworx/gift-cards-veniapwa/src/features/Category/modify/targets/wrapUseGalleryItem"
         );
     });
 };
